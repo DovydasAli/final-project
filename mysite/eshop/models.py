89e303210685dd4e612b78fdabc4c23e3fe50277
DovydasAli <dovydas.ali@gmail.com>
2021-07-05 13:05:03 +0300

    model changes added sub categories removed redundant file

--- mysite/eshop/models.py
+++ mysite/eshop/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField('Name', max_length=200)
     unique_id = models.CharField('Unique id', max_length=20, help_text='Randomly generated product id code')  # bandyti padaryt unikalu id jei neiseis istrynt
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    sub_category = models.ForeignKey('SubCategory', on_delete=models.SET_NULL, null=True)
     description = models.TextField('Description', max_length=1000, help_text='Short description of the product')
     price = models.FloatField('Price', help_text='Price of the product')
 
@@ -13,6 +14,13 @@ class Product(models.Model):
 
 class Category(models.Model):
     name = models.CharField('Name', max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class SubCategory(models.Model):
+    name = models.CharField('Name', max_length=200)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.name}"
