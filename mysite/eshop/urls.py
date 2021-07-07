677367aac71d88c08b07d9e7fd9d480541ba3892
DovydasAli <dovydas.ali@gmail.com>
2021-07-07 13:09:29 +0300

    added products page fixed css

--- mysite/eshop/urls.py
+++ mysite/eshop/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    path('products/<int:product_id>', views.products, name='product'),
 ]
--- mysite/eshop/views.py
+++ mysite/eshop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category, SubCategory, OrderProduct, Order
 
 def index(request):
@@ -17,3 +19,16 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+def products(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    print(products)
+    return render(request, 'products.html', context=context)
+
+def product(request, product_id):
+    single_product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product.html', {'product': single_product})
