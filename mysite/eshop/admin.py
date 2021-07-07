9fab92d27179c29ecb29471cca204ae76169fc4c
DovydasAli <dovydas.ali@gmail.com>
2021-07-07 11:01:03 +0300

    added all admin pages

--- mysite/eshop/admin.py
+++ mysite/eshop/admin.py
@@ -4,8 +4,20 @@ from .models import Product, Category, SubCategory, OrderProduct, Order
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'unique_id', 'category', 'sub_category', 'description', 'price')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name'
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = 'product'
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('product', 'date_created')
+
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Category)
-admin.site.register(SubCategory)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(OrderProduct)
 admin.site.register(Order)
