8e5b20e7b1c2c07f466824947f5e18c4a07947a2
DovydasAli <dovydas.ali@gmail.com>
2021-07-07 09:12:45 +0300

    added unique id and sub-category to admin

--- mysite/eshop/admin.py
+++ mysite/eshop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Category, OrderProduct, Order
+from .models import Product, Category, SubCategory, OrderProduct, Order
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(SubCategory)
 admin.site.register(OrderProduct)
 admin.site.register(Order)
--- mysite/eshop/models.py
+++ mysite/eshop/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 class Product(models.Model):
     name = models.CharField('Name', max_length=200)
-    unique_id = models.CharField('Unique id', max_length=20, help_text='Randomly generated product id code')  # bandyti padaryt unikalu id jei neiseis istrynt
+    unique_id = models.CharField('Unique id', max_length=100, blank=True, unique=True, default=uuid.uuid4)  # bandyti padaryt unikalu id jei neiseis istrynt
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     sub_category = models.ForeignKey('SubCategory', on_delete=models.SET_NULL, null=True)
     description = models.TextField('Description', max_length=1000, help_text='Short description of the product')
@@ -18,12 +19,20 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 class SubCategory(models.Model):
     name = models.CharField('Name', max_length=200)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Sub category'
+        verbose_name_plural = 'Sub categories'
 
 class OrderProduct(models.Model):
     product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
